d05b7a6991b839e454daf8623e8f4bf821699c84
bohdilong <98560374+bohdilong@users.noreply.github.com>
2022-02-24 11:57:12 -0800

    added variables and printed them

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+document.write("Hello, I am the son of anton")
+window.alert("hello, world!")
+var x = "this is a string."
+document.write(x)
+var myVariable = "this is to show case how a variable works"
+document.write(myVariable)
+var story = "Loki said, \"I\'ve been falling for 30 minutes\""
+document.write(story)
--- Basic_JavaScript_Projects/Project1_expressions_alert/main.js
+++
@@ -1,1 +0,0 @@
-alert("Hello, I am Son of Anton.")
