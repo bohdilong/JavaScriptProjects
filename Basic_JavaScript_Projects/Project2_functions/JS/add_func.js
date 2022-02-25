6d710dbcf2b45efb3d7c71907bdc5c91c075bead
bohdilong <98560374+bohdilong@users.noreply.github.com>
2022-02-25 13:55:41 -0800

    function practice

---
+++ Basic_JavaScript_Projects/Project2_functions/JS/add_func.js
@@ -0,0 +1,12 @@
+
+function Add() { //adds 2 and 2 together
+    var Add = 2+2;
+    document.getElementById("addition").innerHTML = Add;
+    
+}
+
+function myFunction() { // concatenates two strings
+    var sentence = "I am learning";
+        sentence += " a lot form this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;    
+}
