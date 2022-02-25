395c784c72706cfce8a14c06b672dba38a003904
bohdilong <98560374+bohdilong@users.noreply.github.com>
2022-02-24 16:23:02 -0800

    Update main.js

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,8 +1,20 @@
-document.write("Hello, I am the son of anton")
-window.alert("hello, world!")
-var x = "this is a string."
-document.write(x)
-var myVariable = "this is to show case how a variable works"
-document.write(myVariable)
-var story = "Loki said, \"I\'ve been falling for 30 minutes\""
-document.write(story)
+document.write("Hello, I am the son of anton") //this displays my name
+window.alert("hello, world!")// dropdown window that prints hello world
+var x = "this is a string."//first variable
+document.write(x)//printing variable
+var myVariable = "this is to show case how a variable works"//assigning the variable
+document.write(myVariable)//calling the variable myVariable
+var story = "Loki said, \"I\'ve been falling for 30 minutes\""//showcasing how to use quotes in a sentence
+document.write(story) //calling the variable story
+document.write("i am in the prosses of selling my house."//adding two strings together
++ "and haveing a kid")
+var catString = "string" + "s"//adding two strings together inside a variable
+document.write(catString)//calling the varaible catstring
+var family = "The Gonzalez's", dad = "manuel", mom = "emily", sister = "rosa", brother1 = "adam",
+ brother2="kevin"//assigning multiple variables at once
+
+ document.write(sister)//printing sister var
+ document.write(3+3); //adds 3+3
+ var text = "hello world";
+ document.write(text.fontcolor("green" ));//this to change the color green
+ alert(text.fontcolor("green"));
