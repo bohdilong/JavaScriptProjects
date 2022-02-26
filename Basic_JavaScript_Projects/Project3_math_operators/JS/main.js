6ad0318955e7287d334f158bcac8131372409c43
bohdilong <98560374+bohdilong@users.noreply.github.com>
2022-02-26 12:23:18 -0800

    Update main.js

--- Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
+++ Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
@@ -1,11 +1,11 @@
 function ADD() {                
     var addition = 10+10;       
-    document.getElementById("add").innerHTML = "10+10=" + addition;
+    document.getElementById("add").innerHTML = "10+10=" + addition;//function that adds 10+10
 }
 
 function SUB() {
     var subtraction = 20-10;
-    document.getElementById("sub").innerHTML = "20-10=" + subtraction;
+    document.getElementById("sub").innerHTML = "20-10=" + subtraction;//funtion that subtracts 10 from 20
 }
 
 function MULTI() {
@@ -20,24 +20,24 @@ function DIVIDE() {
 
 function modulusOperator() {
     var remainder = 60%7;
-    document.getElementById("remain").innerHTML = "60 devided by 7 has a remainder of " + remainder;
+    document.getElementById("remain").innerHTML = "60 devided by 7 has a remainder of " + remainder;// shows the remainder of 60 divided by 7
 }
 
 function unary() {
     var negative = 10;
-    document.getElementById("neg").innerHTML = "The opposite of +10 is " + -negative;
+    document.getElementById("neg").innerHTML = "The opposite of +10 is " + -negative;//shows the negavite of 10
 }
 
 function AddByOne() {
     var x = 5;
     x++
-    document.getElementById("increase").innerHTML = "5+1="+ x;
+    document.getElementById("increase").innerHTML = "5+1="+ x;// 1 increment from 5
 }
 
 function MinusOne() {
     var y = 7;
     y--
-    document.getElementById("decrease").innerHTML = "7 minus 1 is " + y;
+    document.getElementById("decrease").innerHTML = "7 minus 1 is " + y;// 1 decrement from 7
 }
 
-window.alert(Math.random(),10 * 50);
+window.alert((Math.random() * 50)+10);// displays random between 10 and 50 
