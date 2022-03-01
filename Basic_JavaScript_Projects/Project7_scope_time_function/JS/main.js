ed5a9a5fcd5b9288d0d2a5017c9749c2019ba35e
bohdilong <98560374+bohdilong@users.noreply.github.com>
2022-03-01 13:35:24 -0800

    testing global variables
    func sum_1 and sum_2

---
+++ Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,9 @@
+var x = 24;// global variable
+function sum_1() {// adds x and y variables
+    var y = 20;
+    document.write(y + x + "<br>");
+}
+
+function sum_2() {// trys to add x and y
+    document.write(y + x + "<br>")
+}
