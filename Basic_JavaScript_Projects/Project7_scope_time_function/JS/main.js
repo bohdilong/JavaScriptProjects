e2b50e94a859368962bd34f49c41ea859498ff7d
bohdilong <98560374+bohdilong@users.noreply.github.com>
2022-03-01 18:37:31 -0800

    gegsdfs

--- Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
+++ Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
@@ -1,9 +1,24 @@
 var x = 24;// global variable
 function sum_1() {// adds x and y variables
     var y = 20;
-    document.write(y + x + "<br>");
+    document.write(y + x );
 }
 
 function sum_2() {// trys to add x and y
-    document.write(y + x + "<br>")
+    document.write(y + x );
+}
+
+function GetDate() {
+    if (new Date().getHours() < 18) {
+        document.getElementById("Greeting").innerHTML = "How are you today?";
+    }
+}
+
+function WorkDay() {
+    if (new Date().getDay() < 3) {
+        document.getElementById("work").innerHTML = "How was your weekend?";
+    }
+    if (new Date().getDay() >= 4) {
+        document.getElementById("work").innerHTML = "what are you doing here on your day off?"
+    }
 }
